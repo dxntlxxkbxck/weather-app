9f75fa77b4eabf4ea38069bf9c2769eda154c566
Michael <dxntlxxkbxck@gmail.com>
2026-02-01 16:21:01 +0300

    js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const weatherIconMap = {
     '50n': 'water'
 };
 
-function fetchWeatherData(location) {
+const fetchWeatherData = (location) => {
 	const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
 
 	fetch(apiUrl).then(response => response.json()).then(data => {
@@ -38,5 +38,75 @@ function fetchWeatherData(location) {
 		todayInfo.querySelector('span').textContent = new Date().toLocaleDateString('ru', { day: 'numeric', month: 'long', year: 'numeric' });
 		todayWeatherIcon.className = `bx bx-${weatherIconMap[todayWeatherIconCode]}`;
 		todayTemp.textContent = todayTemperature;
-	})
+
+		const locationElement = document.querySelector('today-info > div > span');
+		locationElement.textContent = `${data.city.name}, ${data.city.country}`;
+
+		const weatherDescriptionElement = document.querySelector('.today-weather > h3');
+		weatherDescriptionElement.textContent = todayWeather;
+
+		const todayPrecipitation = `${data.list[0].pop}%`;
+		const todayHumidity = `${data.list[0].main.humidity}%`;
+		const todayWindSpeed = `${data.list[0].wind.speed} km/h`;
+
+		const dayInfoContainer = document.querySelector('.day-info');
+		dayInfoContainer.innerHTML = `
+		
+			<div>
+				<span class="title">PRECIPITATION</span>
+				<span class="value">${todayPrecipitation}</span>
+			</div>
+			<div>
+				<span class="title">HUMIDITY</span>
+				<span class="value">${todayHumidity}</span>
+			</div>
+			<div>
+				<span class="title">WIND SPEED</span>
+				<span class="value">${todayWindSpeed}</span>
+			</div>
+			
+		`;
+
+		const today = new Date();
+		const nextDaysData = data.list.slice(1);
+
+		const uniqueDays = new Set();
+		let count = 0;
+		daysList.innerHTML = '';
+		for (const dayData of nextDaysData){
+			const forecastDate = new Date(dayData.dt_txt);
+			const dayAbbreviation = forecastDate.toLocaleDateString('ru', { weekday: 'short' });
+			const dayTemp = `${Math.round(dayData.main.temp)}°C`;
+			const iconCode = dayData.weather[0].icon;
+
+			if (!uniqueDays.has(dayAbbreviation) && forecastDate.getDate() !== today.getDate()){
+				uniqueDays.add(dayAbbreviation);
+				daysList.innerHTML += `
+				
+					<li>
+						<i class='bx bx-${weatherIconMap[iconCode]}'></i></i>;
+						<span>${dayAbbreviation}</span>
+						<span class="day-temp">${dayTemp}</span>
+					</li>
+				`;
+				count++;
+			}
+
+			if (count === 4) break;
+		}
+	}).catch(error => {
+		alert(`Error fetching weather data: ${error}(Api Error)`);
+	});
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const defaultLocation = 'Russia';
+	fetchWeatherData(defaultLocation);
+});
+
+locButton.addEventListener('click', () => {
+	const location = prompt('Введите название города: ');
+	if (!location) return;
+
+	fetchWeatherData(location);
+})
